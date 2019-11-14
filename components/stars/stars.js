f828e34b357507d3820d286a581b8133f5ddf1a6
wjianhua1878 <943536232@qq.com>
2019-11-14 20:57:14 +0800

    2019/11/14
    首页

---
+++ components/stars/stars.js
@@ -0,0 +1,66 @@
+// components/stars/stars.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    score: {
+      type: Number,
+      value: 0
+    },
+    starSize:{
+      type:Number,
+      value:25 //rpx
+    },
+    fontSize:{
+      type:Number,
+      value:25 //rpx
+    },
+    fontColor:{
+      type:String,
+      value:'#ccc'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  },
+  /**
+   * 组件的生命周期
+   */
+  lifetimes: {
+    attached: function() { // 在组件实例进入页面节点树时执行
+      let _this = this;
+      // console.log(_this.properties.score);
+      let score = _this.properties.score;
+      score =  score <=10 ? score : 10;
+      // console.log(score);
+      let scoreText = (score && (score>0)) ? score.toFixed(1) : "未评分";
+      
+      let intScore = parseInt(score);
+      //满星的星星数量
+      let star1 = parseInt(intScore / 2);
+      //半颗的星星数量
+      let star2 = parseInt(intScore % 2);
+      //灰色星星的数量
+      let star3 = 5 - star1 - star2;
+      // console.log(star1,star2,star3);
+      _this.setData({
+        star1,
+        star2,
+        star3,
+        scoreText
+      })
+    }
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,45 @@ const app = getApp()
 
 Page({
   data: {
-   
-  },
+    movies:[],//电影列表
+    tv:[],//电视
+    varietyShow:[]//综艺
 
+  },
+  onLoad: function() {
+    wx.request({//电影
+      url: 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items',
+      success: res => {
+        console.log(res);
+        console.log(res.data.subject_collection_items);
+        let movieArr = res.data.subject_collection_items;//[{},{}]
+        this.setData({
+          movies: movieArr
+        })
+      }
+    });
+    wx.request({//电视剧
+      url: 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_hot/items',
+      success: res => {
+        console.log(res);
+        console.log(res.data.subject_collection_items);
+        let tvArr = res.data.subject_collection_items;//[{},{}]
+        this.setData({
+          tv: tvArr
+        })
+      }
+    }); 
+    wx.request({//综艺
+      url: 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_variety_show/items',
+      success: res => {
+        console.log(res);
+        console.log(res.data.subject_collection_items);
+        let varietyShowArr = res.data.subject_collection_items;//[{},{}]
+        this.setData({
+          varietyShow: varietyShowArr
+        })
+      }
+    });
+    console.log(this.data);
+  }
 })
